070b58dff403d00e8f496be5177077bdf7afcf1b
Jack Sullivan <jack@divergent.codes>
2025-08-24 20:51:29 -0700

    chore: factor out lambda zip config

---
+++ workspace/lambda_zip.tf
@@ -0,0 +1,22 @@
+########################################################
+# Lambda Zip
+########################################################
+
+module "lambda_application_zip" {
+  count = var.lambda_deployment_type == "zip" ? 1 : 0
+
+  source = "../modules/lambda"
+
+  region               = var.region
+  function_name        = "lambda-basic-${random_string.suffix.result}"
+  function_description = "Demo function for split application & infrastructure"
+  handler              = "index.handler"
+  runtime              = "python3.12"
+
+  github_owner       = var.github_owner
+  github_repo        = var.github_repo
+  github_environment = var.github_environment
+  github_region      = var.github_region
+
+  ecr_repository_name = var.ecr_repository_name
+}
--- workspace/main.tf
+++ workspace/main.tf
@@ -27,26 +27,3 @@ resource "aws_iam_openid_connect_provider" "github" {
 data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
 }
-
-########################################################
-# Lambda Basic
-########################################################
-
-module "lambda_application_zip" {
-  count = var.lambda_deployment_type == "zip" ? 1 : 0
-
-  source = "../modules/lambda"
-
-  region               = var.region
-  function_name        = "lambda-basic-${random_string.suffix.result}"
-  function_description = "Demo function for split application & infrastructure"
-  handler              = "index.handler"
-  runtime              = "python3.12"
-
-  github_owner       = var.github_owner
-  github_repo        = var.github_repo
-  github_environment = var.github_environment
-  github_region      = var.github_region
-
-  ecr_repository_name = var.ecr_repository_name
-}
